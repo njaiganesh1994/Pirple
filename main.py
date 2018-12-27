8fea688038703888af32e46eb370940ffd84a109
njaiganesh1994 <jai.g.nagabooshanam@gmail.com>
2018-12-27 17:28:30 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+#Attributes about a song
+#Variable Assignment
+singer = "Ed Sheeran"
+genre = "pop"
+RuntimeInMinutes = 233
+year = "2017"
+producer = "Ed Sheeran, Steve Mac"
+studio = "Rock Stone Studio"
+views = 200.5
+
+#Printing the Variables
+print(singer)
+print(genre)
+print(RuntimeInMinutes)
+print(year)
+print(producer)
+print(studio)
+print(views)
+
+"""
+I am a the 9th all-time popular song
+
+"""
+
+
+
