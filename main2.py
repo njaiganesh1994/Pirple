35a8b4e33a01b3dcf9be1b9bccbf0d3a21b0e882
njaiganesh1994 <jai.g.nagabooshanam@gmail.com>
2018-12-28 20:12:23 +0530

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,30 @@
+#Attributes about a song
+#Variable Assignment
+singer = "Ed Sheeran"
+genre_1 = "pop"
+year_1 = "2017"
+like=True
+
+
+#Function Definition
+def genre():
+	print(genre_1)
+	return 
+
+def artist():
+    print(singer)
+    return
+
+def year():
+    print(year_1)
+    return
+
+def liking():
+	print(like)
+	return
+
+#Function Calling
+genre()
+artist()
+year()
+liking()
