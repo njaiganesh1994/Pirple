ad32575316752b66ad0728424c64a072bb8c2c42
njaiganesh1994 <jai.g.nagabooshanam@gmail.com>
2019-01-02 15:50:10 +0530

    Add files via upload

---
+++ main3.py
@@ -0,0 +1,15 @@
+##Declaring the variables to undergo the conditional statement
+a=6
+b=5
+c="5"
+##Setting up the output variables
+Succ=True
+Fail=False
+
+if a==b or a==int(c) or b==int(c):
+	print(Succ)
+else:
+    print(Fail)
+
+
+    
